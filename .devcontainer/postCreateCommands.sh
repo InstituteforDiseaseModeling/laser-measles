79136c074589db7192d0ce4647f396ebb48c41b8
Christopher Lorton <christopher.lorton@gatesfoundation.org>
2024-12-04 02:53:15 +0000

    include ipykernel in nb dependencies

--- .devcontainer/postCreateCommands.sh
+++ .devcontainer/postCreateCommands.sh
@@ -2,5 +2,4 @@ pip install --upgrade pip > /tmp/pip.stdout.log 2> /tmp/pip.stderr.log
 pip install uv > /tmp/uv.stdout.log 2> /tmp/uv.stderr.log
 uv tool install tox --with tox-uv > /tmp/tox.stdout.log 2> /tmp/tox.stderr.log
 uv venv --python 3.10 .venv > /tmp/venv.stdout.log 2> /tmp/venv.stderr.log
-.venv/bin/activate
-uv pip install -e . > /tmp/install.stdout.log 2> /tmp/install.stderr.log
+chmod +x .venv/bin/activate && .venv/bin/activate && uv pip install -e .[nb] > /tmp/install.stdout.log 2> /tmp/install.stderr.log
