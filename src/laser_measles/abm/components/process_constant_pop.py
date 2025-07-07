39606f68a61b2ded75fda05a4834b8e51f7c796a
Katherine <krosenf@gmail.com>
2025-07-07 22:28:21 +0000

    Add ConstantPopProcess component for managing births in constant population models
    This commit introduces the ConstantPopProcess class, which handles birth events in models where births equal deaths. It includes parameter definitions, initialization logic, and methods for processing births and updating population states. The new component enhances the simulation framework by providing a structured approach to manage population dynamics.

---
+++ src/laser_measles/abm/components/process_constant_pop.py
@@ -0,0 +1,108 @@
+import numpy as np
+from pydantic import BaseModel
+from pydantic import Field
+
+from laser_measles.abm.model import ABMModel
+from laser_measles.base import BasePhase
+from laser_measles.utils import cast_type
+
+
+class ConstantPopParams(BaseModel):
+    """Parameters specific to the births process component."""
+
+    cbr: float = Field(default=20, description="Crude birth rate per 1000 people per year", ge=0.0)
+
+
+class ConstantPopProcess(BasePhase):
+    """
+    A component to handle the birth events in a model with constant population - that is, births == deaths.
+
+    Attributes:
+
+        model: The model instance containing population and parameters.
+        verbose (bool): Flag to enable verbose output. Default is False.
+        initializers (list): List of initializers to be called on birth events.
+        metrics (DataFrame): DataFrame to holding timing metrics for initializers.
+    """
+
+    def __init__(self, model: ABMModel, verbose: bool = False, params: ConstantPopParams | None = None):
+        """
+        Initialize the Births component.
+
+        Parameters:
+
+            model (object): The model object which must have a `population` attribute.
+            verbose (bool, optional): If True, enables verbose output. Defaults to False.
+            params (BirthsParams, optional): Component parameters. If None, uses model.params.
+
+        Raises:
+
+            AssertionError: If the model does not have a `population` attribute.
+            AssertionError: If the model's population does not have a `dob` attribute.
+        """
+
+        super().__init__(model, verbose)
+
+        self.params = params if params is not None else ConstantPopParams()
+
+        model.people.add_scalar_property("dob", dtype=np.int32)
+
+        model.patches.add_scalar_property("births", dtype=np.uint32)
+
+        return
+
+    def initialize(self, model: ABMModel) -> None:
+        people = model.people
+
+        # Simple initializer for ages where birth rate = mortality rate:
+        daily_mortality_rate = (1 + self.params.cbr / 1000) ** (1 / 365) - 1
+        # Initialize ages for existing population
+        people.dob[0 : people.count] = cast_type(-1 * model.prng.exponential(1 / daily_mortality_rate, people.count), people.dob.dtype)
+
+
+    @property
+    def mu(self) -> float:
+        """daily mortality rate"""
+        return (1 + self.params.cbr / 1000) ** (1 / 365) - 1
+
+    def __call__(self, model, tick) -> None:
+        """
+        Adds new agents to each patch based on expected daily births calculated from CBR. Calls each of the registered initializers for the newborns.
+
+        Args:
+
+            model: The simulation model containing patches, population, and parameters.
+            tick: The current time step in the simulation.
+
+        Returns:
+
+            None
+
+        This method performs the following steps:
+
+            1. Draw a random set of indices, or size size "number of births"  from the population,
+        """
+
+        patches = model.patches
+        people = model.people
+        populations = patches.states[:-1,:].sum(axis=0) # exclude deaths
+
+        # When we get to having birth rate per node, will need to be more clever here, but with constant birth rate across nodes,
+        # random selection will be population proportional.  If node id is not contiguous, could be tricky?
+        births = model.prng.poisson(lam=populations * self.mu, size=populations.shape)
+        idx = model.prng.choice(populations.sum(), size=births.sum(), replace=False)
+
+        # Get number of deaths per patch per state
+        num_states = len(model.params.states)
+        num_patches = len(patches)
+        deaths = np.bincount(people.state[idx]*num_patches + people.patch_id[idx], minlength=num_patches*num_states)
+        deaths = deaths.reshape((num_states, num_patches))
+
+        # update state counters
+        patches.states -= cast_type(deaths, patches.states.dtype)
+        patches.states.S += cast_type(births, patches.states.dtype)
+
+        # Births, set date of birth and state to 0 (susceptible)
+        people.dob[idx] = tick  # set to current tick
+        people.state[idx] = model.params.states.index("S") # set to susceptible
+
