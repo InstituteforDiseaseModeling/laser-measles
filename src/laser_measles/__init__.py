ab5c81243e69de42ceb0b7689d52ed2cd94d7144
Katherine Rosenfeld <krosenf@gmail.com>
2025-07-16 14:54:39 +0000

    Bump version: 0.7.0-dev0 → 0.7.1-dev0

--- src/laser_measles/__init__.py
+++ src/laser_measles/__init__.py
@@ -6,7 +6,7 @@ using agent-based and compartmental models with various spatial and temporal con
 """
 
 # ruff: noqa: F401, F403, E402
-__version__ = "0.7.0-dev0"
+__version__ = "0.7.1-dev0"
 
 # --- Exports ---
 MEASLES_MODULES = ["laser_measles.abm", "laser_measles.compartmental", "laser_measles.biweekly"]
